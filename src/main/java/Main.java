6c254f2ef9f037dfc5380897c6376ba7370f4a62
LeadGameV2 <leadgamev2@gmail.com>
2022-07-08 01:03:45 +0300

    trywithresourses added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,15 +9,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
         int port = 80;
         while (true) {
-            ServerSocket serverSocket = new ServerSocket(port);
-            Socket clientSocket = serverSocket.accept();
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            try (ServerSocket serverSocket = new ServerSocket(port);
+                 Socket clientSocket = serverSocket.accept();
+                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
 
-            System.out.printf("New conected accepted. Port: %d%n", clientSocket.getPort());
-            String name = in.readLine();
-            out.println(String.format("Hi, %s, your port is %d", name, clientSocket.getPort()));
-            serverSocket.close();
+                System.out.printf("New conected accepted. Port: %d%n", clientSocket.getPort());
+                String name = in.readLine();
+                out.println(String.format("Hi, %s, your port is %d", name, clientSocket.getPort()));
+
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
         }
 
 
